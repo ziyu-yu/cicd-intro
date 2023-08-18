e86a3e90e48b7416990a7bc8bb8e4703110bc74b
Ricardo <132640888+ricardo7364@users.noreply.github.com>
2023-08-18 23:09:36 +0000

    upatead

--- script.sh
+++ script.sh
@@ -4,14 +4,14 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
 done
 # sudo systemctl start docker
 # sudo systemctl enable docker
-sudo groupadd docker
-sudo usermod -aG docker jenkins
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# sudo groupadd docker
+# sudo usermod -aG docker jenkins
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o ./docker-compose
+# chmod +x /usr/local/bin/docker-compose
 
 cd /home/ubuntu/
 echo "ECR_REPOSITORY=$ECR_REPOSITORY 
 CONTAINER_NAME=$CONTAINER_NAME" >> .env
 
 docker image  prune -f
-docker-compose --env-file=.env pull && docker-compose --env-file=.env up -d
+./docker-compose --env-file=.env pull && docker-compose --env-file=.env up -d
