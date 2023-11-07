6cd47647e733f9f177c297bdfbbf6df6755db67c
Ricardo <132640888+ricardo7364@users.noreply.github.com>
2023-11-07 14:32:28 +0000

    udpated

--- aws-cicd/scripts/setup.sh
+++ aws-cicd/scripts/setup.sh
@@ -4,4 +4,5 @@ key_secret=$(echo $AWS_KEY_ID | jq '.aws_key_secret' | tr -d '"')
 export AWS_ACCESS_KEY_ID=$key_id
 export AWS_SECRET_ACCESS_KEY=$key_secret
 export AWS_REGION=$REGION
+echo $key_id
 aws sts get-caller-identity
