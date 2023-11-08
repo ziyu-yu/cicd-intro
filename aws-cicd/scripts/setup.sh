62e08cf6a5f19e9096ed08bb766090b1fd6c320b
Ricardo <132640888+ricardo7364@users.noreply.github.com>
2023-11-08 18:11:01 +0000

    updaed

--- aws-cicd/scripts/setup.sh
+++ aws-cicd/scripts/setup.sh
@@ -12,8 +12,10 @@ echo $EKS_CODEBUILD_ROLE_ARN
 helm version
 mkdir ~/.kube/
 aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $EKS_CLUSTER_NAME
+key_id=$(echo $AWS_KEY_ID | jq '.aws_key_id' | tr -d '"') 
+key_secret=$(echo $AWS_KEY_ID | jq '.aws_key_secret' | tr -d '"')
+export AWS_ACCESS_KEY_ID=$key_id
+export AWS_SECRET_ACCESS_KEY=$key_secret
 kubectl get nodes
-sudo kubectl version
-chmod 0600 ~/.kube/config
 kubectl version --output=json
 echo "Setup Done !!"
